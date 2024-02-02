8316fbdf73fd820e37c09b9b8718d39f168bb885
SPCruzaley <sp_cruzaley@hotmail.com>
2024-02-02 06:28:53 -0600

    Mensaje del commit

---
+++ src/main/java/com/ebac/modulo39/Modulo39Application.java
@@ -0,0 +1,13 @@
+package com.ebac.modulo39;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo39Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modulo39Application.class, args);
+	}
+
+}
